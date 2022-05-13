a0875d005c1ca5405d19bd2950cfa9e85c0d19ec
Luke Templeman <luke1.temp20@gmail.com>
2022-05-13 20:28:05 +0200

    Started using Colly library

--- fightingMechanics.go
+++ fightingMechanics.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func takeDamage(currenthealth int, damageTaken int) int {
 	healthFinal := currenthealth - damageTaken
@@ -11,7 +15,38 @@ func takeDamage(currenthealth int, damageTaken int) int {
 }
 
 func gameOver() {
+
 	fmt.Println("Welcome to the tutorial")
 	fmt.Println("Welcome to the tutorial")
-	fmt.Println("Welcome to the tutorial")
+}
+
+func randFirearm() {
+	c := colly.NewCollector()
+
+	c.OnHTML("div", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.Visit("https://perchance.org/pi96s9irte")
+
+}
+
+func randMelee() {
+
+}
+
+func randSecret() {
+
+}
+
+func randBodyPart() {
+	link := "https://perchance.org/body-part"
+}
+
+func randLocation() {
+
+}
+
+func randEnemy() {
+	link := "https://perchance.org/monster-type"
 }
--- tutorial.go
+++ tutorial.go
@@ -1,5 +1,47 @@
 package main
 
+import "fmt"
+
 func tutorialStart() {
+	fmt.Println("Welcome to the tutorial")
+	fmt.Println("Here you will learn the basics of fighting the undead")
+	fmt.Println("To get started , a random weapon will spawn")
+	fmt.Println("You may choose the catagory of weapon, but not the actual weapon itself...")
+	fmt.Println("1) Melee Weapons")
+	fmt.Println("2) Firearms ")
+	fmt.Println("3) Secret Weapons....")
+
+	var answer string
+	var memeChoice string
+
+	for answer != "1" && answer != "2" && answer != "3" {
+		fmt.Print(":")
+		fmt.Scan(&answer)
+
+		switch answer {
+		case "1":
+			randFirearm()
+		case "2":
+			randFirearm()
+		case "3":
+			fmt.Print("Are you sure?? (y/n)")
+			fmt.Scan(&memeChoice)
+			if memeChoice == "y" {
+				fmt.Println("Mwahahaha wise choice")
+			} else {
+				fmt.Println("too bad.. lets begin")
+			}
+
+		default:
+			fmt.Println("Not an answer... Try again")
+		}
+
+	}
+
+}
+
+func levelStart() {
+	fmt.Println("too bad.. lets begin")
+	fmt.Println("too bad.. lets begin")
 
 }
